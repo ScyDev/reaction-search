e70d2c1d514cf7d40651d4e18f4c384b5243185b
ScyDev <lukas.saegesser@gmail.com>
2016-04-27 21:33:48 +0200

    - show filter date picker in german - autoclose, because that's what users expect

--- client/templates/searchBox.js
+++ client/templates/searchBox.js
@@ -8,8 +8,23 @@ Template.searchBox.onCreated(
 
 Template.searchBox.onRendered(
 	function() {
-	  $('#searchBoxDate').datepicker({
+		$.fn.datepicker.dates['de'] = {
+			days: ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"],
+			daysShort: ["Son", "Mon", "Die", "Mit", "Don", "Fre", "Sam"],
+			daysMin: ["So", "Mo", "Di", "Mi", "Do", "Fr", "Sa"],
+			months: ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"],
+			monthsShort: ["Jan", "Feb", "Mär", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dez"],
+			today: "Heute",
+			monthsTitle: "Monate",
+			clear: "Löschen",
+			weekStart: 1,
 			format: "dd.mm.yyyy"
+		};
+
+	  $('#searchBoxDate').datepicker({
+			format: "dd.mm.yyyy",
+			language: "de",
+			autoclose: true
 		});
 
 		// session does not survive page reload?!?
